939ab7bb132c64ca8b900031eee2cd45ca670d03
Victor Duarte <victorctii217@gmail.com>
2022-05-04 18:38:55 -0300

    PageHeader, TodoForm and TodoList components

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About us</h1>
+        <h1><PageHeader name='About' small='Us'></PageHeader></h1>
+
+        <h2> Our History </h2>
+        <p> Lorem Ipsum [...]</p>
+
+        <h2> Our Mission </h2>
+        <p> Lorem Ipsum [...]</p>
+
+        <h2> Press </h2>
+        <p> Lorem Ipsum [...]</p>
     </div>
 )
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <h1>
+                    <PageHeader 
+                        name='Tasks' 
+                        small='Register'>
+                    </PageHeader>
+
+                    <TodoForm />
+                    <TodoList />
+                </h1>
             </div>
         )
     }
