b7a0b90d7514bef718e406c2d6e17cad04ac3d01
Victor Duarte <victorctii217@gmail.com>
2022-05-05 15:52:29 -0300

    Add Event (MongoDB integration)

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,7 +1,11 @@
+import axios from 'axios'
+
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props) {
@@ -14,14 +18,16 @@ export default class Todo extends Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
-    
-    handleAdd() {
-        
-    }
 
     handleChange(e) {
         this.setState({...this.state, description: e.target.value })
     }
+    
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('éan'))
+    }   
 
     render() {
         return (
