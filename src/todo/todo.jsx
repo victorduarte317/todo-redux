82ae0e78636025275a1835b3b504fbbe112e91d7
Victor Duarte <victorctii217@gmail.com>
2022-05-05 16:14:05 -0300

    Delete task button

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -17,6 +17,21 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+        .then(resp => 
+            this.setState({
+                ...this.state, 
+                description: '', 
+                list: resp.data
+            })
+        )
     }
 
     handleChange(e) {
@@ -26,8 +41,13 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('éan'))
-    }   
+            .then(resp => this.refresh())
+    }
+    
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
 
     render() {
         return (
@@ -42,7 +62,8 @@ export default class Todo extends Component {
                         description={this.state.description}
                         handleChange={this.handleChange}
                         handleAdd={this.handleAdd} />
-                    <TodoList />
+                    <TodoList list={this.state.list}
+                        handleRemove={this.handleRemove} />
                 </h1>
             </div>
         )
