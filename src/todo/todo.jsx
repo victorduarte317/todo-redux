11f577a24596665e9f702cd277d355271fb5f8e3
Victor Duarte <victorctii217@gmail.com>
2022-05-05 16:31:02 -0300

    Mark task as done/pending functions

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -20,6 +20,9 @@ export default class Todo extends Component {
 
         this.handleRemove = this.handleRemove.bind(this)
 
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
         this.refresh()
     }
 
@@ -49,6 +52,16 @@ export default class Todo extends Component {
             .then(resp => this.refresh())
     }
 
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true })
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false })
+            .then(resp => this.refresh())
+    }
+
     render() {
         return (
             <div>
@@ -62,8 +75,13 @@ export default class Todo extends Component {
                         description={this.state.description}
                         handleChange={this.handleChange}
                         handleAdd={this.handleAdd} />
-                    <TodoList list={this.state.list}
-                        handleRemove={this.handleRemove} />
+
+                    <TodoList 
+                        list={this.state.list}
+                        handleRemove={this.handleRemove}
+                        handleMarkAsDone={this.handleMarkAsDone}
+                        handleMarkAsPending={this.handleMarkAsPending}
+                    />
                 </h1>
             </div>
         )
