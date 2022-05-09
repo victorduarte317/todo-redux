7a093a9fe3f2291aa559b76600e2ce7b259c9841
Victor Duarte <victorctii217@gmail.com>
2022-05-09 15:26:04 -0300

    Todo App with Redux - Done

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -8,10 +8,14 @@ export const changeDescription = e => ({
 })
 
 export const search = () => {
-    const request = axios.get(`${URL}?sort=-createdAt`)
-    return {
-        type: 'TODO_SEARCHED',
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().todo.description
+        const search = description ? `&description__regex=/${description}/` : ''
+        const request = axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => dispatch({
+                type: 'TODO_SEARCHED',
+                payload: resp.data
+            }))
     }
 }
 
@@ -48,5 +52,5 @@ export const removeTask = (todo) => {
 }
 
 export const clear = () => {
-    return { type: 'TODO_CLEAR' }
+    return [{ type: 'TODO_CLEAR' }, search()]
 }
