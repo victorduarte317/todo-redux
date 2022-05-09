a6250fdbce215beb473b7b845eb9b7030dac955b
Victor Duarte <victorctii217@gmail.com>
2022-05-09 15:08:30 -0300

    Clear form

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux'
 import React, { Component } from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 // git pushaaa
 
@@ -18,11 +18,11 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, search, description } = this.props
+        const { add, clear, search, description } = this.props
         if (e.key === 'Enter') {
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            this.props.handleClear()
+            clear()
         }
     }   
 
@@ -46,7 +46,7 @@ class TodoForm extends Component {
                     onClick={() => search()}></IconButton>
     
                 <IconButton style='default' icon='close'
-                    onClick={this.propshandleClear}></IconButton>
+                    onClick={this.props.clear}></IconButton>
             </Grid>
         </div>
         )
@@ -58,6 +58,6 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => 
-    bindActionCreators({ add, changeDescription, search}, dispatch )
+    bindActionCreators({ add, changeDescription, search, clear}, dispatch )
         
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
